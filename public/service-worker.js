b6e0917865a602375aa71c9a0373f32216a64fc7
Neil Creveling <neil.creveling@gmail.com>
2021-06-03 09:43:21 -0400

    place db.js in service-worker.js to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,6 +3,7 @@ const FILES_TO_CACHE = [
     '/index.html',
     '/index.js',
     '/styles.css',
+    '/db.js',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
     '/manifest.webmanifest',
